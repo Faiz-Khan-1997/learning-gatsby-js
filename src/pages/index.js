ff037ca245bd5d81efa45b5d9cf30d119b8b4c3e
Manindra Gautam <manindra@akaruilabs.com>
2019-01-31 08:33:50 +0530

    learned css module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Header from '../components/header'
+import Container from '../components/container'
 
-export default () => <div style={{ color: `purple` }}>
+export default () => <Container style={{ color: `purple` }}>
   <Link style={{ marginRight: 10 }} to='/about'>About</Link>
   <Link to='/contact'>Contact</Link>
   <Header headerText='Hello World' />
   <p>I'm spreading</p>
   <img src='https://source.unsplash.com/random/400x200' alt='' />
-</div>
+</Container>

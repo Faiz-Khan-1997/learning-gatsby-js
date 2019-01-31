0cd3b0edcf0bf7d7c53fc01fbc423b0d0b868d5f
Manindra Gautam <manindra@akaruilabs.com>
2019-01-31 08:59:32 +0530

    learned plugin use with gatsby-config.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Container from '../components/container'
 export default () => <Container style={{ color: `purple` }}>
   <Link style={{ marginRight: 10 }} to='/about'>About</Link>
   <Link to='/contact'>Contact</Link>
-  <Header headerText='Hello World' />
+  <Header headerText='Hello World (Typography changed with plugin)' />
   <p>I'm spreading</p>
   <img src='https://source.unsplash.com/random/400x200' alt='' />
 </Container>
